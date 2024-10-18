d48b90fe69cecffeddaedf127568ba0e2f44f1f5
nickdoa <aeioulofi@gmail.com>
2024-10-18 12:34:39 -0500

    Updated Values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motortest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motortest.java
@@ -2,10 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import java.util.ArrayList;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 @TeleOp
 public class motortest extends LinearOpMode {
@@ -19,10 +18,44 @@ public class motortest extends LinearOpMode {
         motor = hardwareMap.get(DcMotor.class, "flipMotor");
 
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setPower(.1);
-        motor.setTargetPosition(526);
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setTargetPosition(0);
-        sleep(1000);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            motor.setTargetPosition(50);
+            motor.setPower(0.5);
+
+            while (motor.isBusy() && opModeIsActive()) {
+                telemetry.addData("Moving to 25", motor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            sleep(1000);
+
+            motor.setTargetPosition(-5);
+            motor.setPower(0.5);
+
+            while (motor.isBusy() && opModeIsActive()) {
+                telemetry.addData("Moving to -25", motor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            sleep(50);
+
+            motor.setTargetPosition(-1);
+            motor.setPower(2);
+
+            // Wait for the small tick move to complete
+            while (motor.isBusy() && opModeIsActive()) {
+                telemetry.addData("Moving slight tick back", motor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Sleep before starting the next cycle
+            sleep(500);
+        }
     }
 }
