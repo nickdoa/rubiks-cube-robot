e698a48dee0e98ffb1dc2cf2a50bfd959d0c99a7
AquR3d <123186046+AquR3d@users.noreply.github.com>
2024-10-18 17:46:30 +0000

    changed Main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -4,14 +4,8 @@ public class Main{
     public static void main(String[] args){
 
         Cube cube = new Cube();
-        Cube cube2 = cube;
-        Cube cube3 = new Cube(cube);
-
-        cube.turn("W'");
-
+        
         System.out.println(cube);
-        System.out.println(cube2);
-        System.out.println(cube3);
 
 
         /*
